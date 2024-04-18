64bcb14788eb6ee2e4fb38f9c61e85007c8de90d
nikhilkadiyan <kadiyannikhil2@gmail.com>
2024-04-18 17:50:42 +0530

    added id input in edit user

--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 const patchUser = require('../apicalls/patchUser');
 
 function EditUser(props) {
-    const [user, updateUser] = useState({ email: '', name: '', mobile: '',id:'112' });
+    const [user, updateUser] = useState({ email: '', name: '', mobile: '', id: '112' });
 
-    const handleUpdateClick = async(e)=>{
+    const handleUpdateClick = async (e) => {
         e.preventDefault();
-        let res = await patchUser(user.email,user.id,user.mobile,user.name);
+        let res = await patchUser(user.email, user.id, user.mobile, user.name);
         props.updateResOutput(res);
         props.setEditUserVisibility(false);
     }
@@ -14,12 +14,16 @@ function EditUser(props) {
     const onChange = (e) => {
         updateUser({ ...user, [e.target.name]: e.target.value })
     }
-  return (
-    <div>
-        <form>
+    return (
+        <div>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="id" className="form-label">Email</label>
+                    <input type="text" className="form-control" id="id" name='id'  onChange={onChange} />
+                </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" onChange={onChange} />
+                    <input type="email" className="form-control" id="email" name='email' onChange={onChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
@@ -30,12 +34,12 @@ function EditUser(props) {
                     <input type="text" className="form-control" id="mobile" name='mobile' maxLength={10} onChange={onChange} />
                 </div>
                 <div className="my-3">
-                    <button onClick={()=>{props.setEditUserVisibility(false)}} type="button" className="btn btn-secondary me-3">Close</button>
+                    <button onClick={() => { props.setEditUserVisibility(false) }} type="button" className="btn btn-secondary me-3">Close</button>
                     <button onClick={handleUpdateClick} type="submit" className="btn btn-primary">Update User</button>
                 </div>
             </form>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default EditUser
